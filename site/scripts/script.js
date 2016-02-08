043dfc786e130b03261cc85faa32c434a0436a12
Siberia SF Mac Pro <macpro@Siberias-Mac-Pro.local>
2016-02-08 11:20:57 -0800

    quote

--- site/scripts/script.js
+++ site/scripts/script.js
@@ -1,3 +1,23 @@
 'use strict';
 
 console.log( 'Javascript.' );
+
+function getData() {
+    var url = "http://query.yahooapis.com/v1/public/yql";
+    var symbol = 'gpro';
+    var data = encodeURIComponent("select * from yahoo.finance.quotes where symbol in ('" + symbol + "')");
+
+    $.getJSON(url, 'q=' + data + "&format=json&diagnostics=true&env=http://datatables.org/alltables.env")
+        .done(function (data) {
+        $("#results>p").text("price: " + data.query.results.quote.LastTradePriceOnly);
+        console.log(data.query.results);
+    })
+        .fail(function (jqxhr, textStatus, error) {
+        var err = textStatus + ", " + error;
+            $("#results>p").text('Request failed: ' + err);
+    });
+}
+getData();
+// setInterval(function(){
+// 	getData();
+// },1000)
