1f2d539bcf664e33027f851ea8940bc8018583ff
Laith <info@laithazzam.com>
2016-02-08 22:39:44 -0800

    fake data

--- site/scripts/script.js
+++ site/scripts/script.js
@@ -21,3 +21,48 @@ getData();
 // setInterval(function(){
 // 	getData();
 // },1000)
+
+function generateData() {
+	var symbol = {
+		price: '12',
+		high: '',
+		low: '',
+		open: ''
+	};
+	symbol.price = Math.floor( Math.random() * 7 );
+	symbol.open = symbol.price;
+	symbol.high = symbol.price;
+	symbol.low = symbol.price;
+	setInterval(function(){
+		$('.lower .high p').text(symbol.open);
+		var change = (Math.random() < 0.5 ? -1 : 1) * (Math.random() * .50);
+		console.log(change);
+		symbol.price = symbol.price + change;
+		console.log(symbol.price);
+		$("#results>p").text("price: " + symbol.price);
+
+		if(symbol.price < symbol.low){
+			symbol.low = symbol.price;
+			$('.lower .low p').text(symbol.low);
+		}
+		else if(symbol.price > symbol.high){
+			symbol.high = symbol.price;
+			$('.upper .high p').text(symbol.high);
+		}
+		else if(symbol.price > symbol.open){
+			$('.candlestick').addClass('pos');
+			$('.candlestick').removeClass('neg');
+		}
+		else if(symbol.price < symbol.open){
+			$('.candlestick').addClass('neg');
+			$('.candlestick').removeClass('pos');
+		}
+		else if(symbol.price == symbol.open){
+			$('.candlestick').removeClass('neg');
+			$('.candlestick').removeClass('pos');
+		}
+
+	},2000);
+	
+}
+generateData();
