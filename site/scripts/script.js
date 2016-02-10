d6cdd29fd1183ddc7caec0a9b00854fa7eed25c1
Laith <info@laithazzam.com>
2016-02-09 23:47:31 -0800

    candle animation and data

--- site/scripts/script.js
+++ site/scripts/script.js
@@ -2,67 +2,94 @@
 
 console.log( 'Javascript.' );
 
-function getData() {
-    var url = "http://query.yahooapis.com/v1/public/yql";
-    var symbol = 'gpro';
+$(document).keypress(function(event) {
+	$(".symbol-input").focus();
+    if (event.keyCode >= 65 && event.keyCode <= 90 || event.keyCode >= 97 && event.keyCode <= 122 ){
+    	if(!$('#symbol-form').hasClass('active')){
+    		$('#symbol-form').addClass('active');
+    		$('.candlestick').addClass('muted');
+    		$(".symbol-input").select();
+    	}
+    }
+    else if(event.keyCode == 13) {
+    	var inputSymbol = $(".symbol-input").val();
+		generateData(inputSymbol);
+    }
+});
+
+$("form").submit(function() { 
+});
+
+function generateData(inputSymbol) {
+	var url = "http://query.yahooapis.com/v1/public/yql";
+    var symbol = inputSymbol;
     var data = encodeURIComponent("select * from yahoo.finance.quotes where symbol in ('" + symbol + "')");
 
     $.getJSON(url, 'q=' + data + "&format=json&diagnostics=true&env=http://datatables.org/alltables.env")
         .done(function (data) {
         $("#results>p").text("price: " + data.query.results.quote.LastTradePriceOnly);
+        $('#symbol-form').removeClass('active');
+        $('.candlestick').removeClass('muted');
+        $('h1').text('$'+symbol);
+    	$('#results').addClass('active');
         console.log(data.query.results);
+        setItems(data);
     })
         .fail(function (jqxhr, textStatus, error) {
         var err = textStatus + ", " + error;
             $("#results>p").text('Request failed: ' + err);
     });
+
+    function setItems(data){
+    	var symbol = {
+			price: data.query.results.quote.LastTradePriceOnly,
+			high: data.query.results.quote.DaysHigh,
+			low: data.query.results.quote.DaysLow,
+			open: data.query.results.quote.Open,
+			prevClose: data.query.results.quote.PreviousClose,
+			upperWick: 0,
+			lowerWick: 0,
+			body: 0,
+			spread: data.query.results.quote.DaysHigh - data.query.results.quote.DaysLow
+		};
+
+		$('.upper .high p').text(symbol.high);
+		$('.lower .low p').text(symbol.low);
+
+		var cHeight = window.innerHeight - (window.innerHeight*.40);
+
+		if( symbol.price > symbol.prevClose  ){
+			$('body').addClass('pos');
+			$('body').removeClass('neg');
+
+			$('.upper .low p').text(symbol.price);
+			$('.lower .high p').text(symbol.prevClose);
+
+			symbol.upperWick = (symbol.high - symbol.price) / symbol.spread;
+			symbol.lowerWick = (symbol.prevClose - symbol.low) / symbol.spread;
+			symbol.body = (symbol.price - symbol.prevClose) / symbol.spread;
+
+			console.log(symbol);
+
+		}else if( symbol.price < symbol.prevClose ){
+			$('body').addClass('neg');
+			$('body').removeClass('pos');
+
+			$('.upper .low p').text(symbol.prevClose);
+			$('.lower .high p').text(symbol.price);
+
+			symbol.upperWick = (symbol.high - symbol.prevClose) / symbol.spread;
+			symbol.lowerWick = (symbol.price - symbol.low) / symbol.spread;
+			symbol.body = (symbol.prevClose - symbol.price) / symbol.spread;
+		}else{
+			$('body').removeClass('neg');
+			$('body').removeClass('pos');
+
+			$('.upper .low p').text(symbol.prevClose);
+			$('.lower .high p').text(symbol.price);
+		}
+		$('.body').css('height',cHeight*symbol.body);
+		$('.upper').css('height',cHeight*symbol.upperWick);
+		$('.lower').css('height',cHeight*symbol.lowerWick);
+    }	
 }
-getData();
-// setInterval(function(){
-// 	getData();
-// },1000)
-
-function generateData() {
-	var symbol = {
-		price: '12',
-		high: '',
-		low: '',
-		open: ''
-	};
-	symbol.price = Math.floor( Math.random() * 7 );
-	symbol.open = symbol.price;
-	symbol.high = symbol.price;
-	symbol.low = symbol.price;
-	setInterval(function(){
-		$('.lower .high p').text(symbol.open);
-		var change = (Math.random() < 0.5 ? -1 : 1) * (Math.random() * .50);
-		console.log(change);
-		symbol.price = symbol.price + change;
-		console.log(symbol.price);
-		$("#results>p").text("price: " + symbol.price);
-
-		if(symbol.price < symbol.low){
-			symbol.low = symbol.price;
-			$('.lower .low p').text(symbol.low);
-		}
-		else if(symbol.price > symbol.high){
-			symbol.high = symbol.price;
-			$('.upper .high p').text(symbol.high);
-		}
-		else if(symbol.price > symbol.open){
-			$('.candlestick').addClass('pos');
-			$('.candlestick').removeClass('neg');
-		}
-		else if(symbol.price < symbol.open){
-			$('.candlestick').addClass('neg');
-			$('.candlestick').removeClass('pos');
-		}
-		else if(symbol.price == symbol.open){
-			$('.candlestick').removeClass('neg');
-			$('.candlestick').removeClass('pos');
-		}
-
-	},2000);
-	
-}
-generateData();
